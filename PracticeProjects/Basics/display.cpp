804072a5c7d2eaf4afd895bd5680dbb6518d74d8
Daniel Dalacan <danbits14@gmail.com>
2026-04-21 13:09:28 +0100

    Add control flow practice - for, while, do while loops

--- PracticeProjects/Basics/display.cpp
+++ PracticeProjects/Basics/display.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 
 int main() {
+    
+    for (int i = 1; i <= 10; i++) {
+        cout << i <<endl;
+    }
+    return 0;
 
-    cout<< "hello world" << endl;
+    // int num = 7;
+    // int guess;
+    // cout<< "enter a number to guess the system nunber";
+    // cin >> guess;
+
+    // while (guess != num) {
+    //     cout << "wrong guess, try again" <<endl;
+    //     cin >> guess;
+    // }
+
+    // cout << "you got the guess";
+
+    // return 0;
+
+    // int num =1;
+
+    // do {
+    //     cout<<num<<endl;
+    //     num++;
+        
+    // } while (num <= 10);
+
 }
